276940f0dbbde7d61361abc9297b14e2ceb6edc3
Radar-Lei <greatradar@gmail.com>
2025-04-01 18:43:32 +0800

    up

--- test.py
+++ test.py
@@ -8,4 +8,7 @@ dataset = load_dataset("neuralwork/arxiver")
 df = dataset['train'].to_pandas()
 
 # Show the first 5 papers with title and abstract
-print(df['markdown'].iloc[0])
+# print(df['markdown'].iloc[0])
+
+# df[['markdown']].to_csv('raw_paper_data/markdown_only.csv', index=False)
+type(df['markdown'].values)
